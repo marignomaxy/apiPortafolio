376f576e29137aa8a79f8925f8828db740cc6e5e
Maximiliano Marigno <marignomaxy@gmail.com>
2023-11-03 03:43:21 -0300

    modificaciones app.js

--- controllers/usuariosControllers.js
+++ controllers/usuariosControllers.js
@@ -1,53 +1,49 @@
-var modeloUsuario = require("../models/usuariosmodels");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+var modeloUsuario = require('../models/usuariosmodels')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-async function compararContrasena(
-  contrasenaIngresada,
-  contrasenaEnBaseDeDatos
-) {
+async function compararContrasena(contrasenaIngresada, contrasenaEnBaseDeDatos) {
   try {
-    return await bcrypt.compare(contrasenaIngresada, contrasenaEnBaseDeDatos);
+    return await bcrypt.compare(contrasenaIngresada, contrasenaEnBaseDeDatos)
   } catch (error) {
-    console.error("Error al comparar contraseñas:", error);
-    return false; // En caso de error, asumimos que las contraseñas no coinciden
+    console.error('Error al comparar contraseñas:', error)
+    return false // En caso de error, asumimos que las contraseñas no coinciden
   }
 }
 
 module.exports = {
   getByEmail: async function (req, res, next) {
-    const { mail, contraseña } = req.body;
+    const { mail, contraseña } = req.body
     try {
-      let usuario = await modeloUsuario.getByEmail(mail);
+      let usuario = await modeloUsuario.getByEmail(mail)
 
       if (await compararContrasena(contraseña, usuario.contraseña)) {
         const payload = {
           usuario: usuario.mail, // Cambia esto por los datos que desees incluir
-        };
-        const secretKey = process.env.SECRET_KEY;
+        }
+        const secretKey = process.env.SECRET_KEY
         const options = {
-          expiresIn: "1h", // Tiempo de expiración del token (por ejemplo, 1 hora)
-        };
-        const token = jwt.sign(payload, secretKey, options);
-        console.log("Usuario:", usuario, " Token:", token);
-        res.status(200).json({ usuario, token });
+          expiresIn: '1h', // Tiempo de expiración del token (por ejemplo, 1 hora)
+        }
+        const token = jwt.sign(payload, secretKey, options)
+        res.status(200).json({ usuario, token })
       } else {
-        res.status(401).json({ error: "Contraseña incorrecta" });
+        res.status(401).json({ error: 'Contraseña incorrecta' })
       }
     } catch (error) {
-      console.error("Error al obtener usuarios por email:", error);
-      res.status(500).json({ error: "Error interno del servidor" });
+      console.error('Error al obtener usuarios por email:', error)
+      res.status(500).json({ error: 'Error interno del servidor' })
     }
   },
 
   create: async function (req, res, next) {
-    console.log(req.body);
+    console.log(req.body)
     try {
-      await modeloUsuario.create(req.body);
-      res.status(201).json("Usuario Creado");
+      await modeloUsuario.create(req.body)
+      res.status(201).json('Usuario Creado')
     } catch (error) {
-      console.error("Error al obtener usuarios por email:", error);
-      res.status(500).json({ error: "Error interno del servidor" });
+      console.error('Error al obtener usuarios por email:', error)
+      res.status(500).json({ error: 'Error interno del servidor' })
     }
   },
-};
+}
