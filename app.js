4dbb7cf0fdc27e855cbbc610d01f3bf8f2f0e671
Maximiliano Marigno <marignomaxy@gmail.com>
2023-11-03 01:28:05 -0300

    agregar vercel.json4

--- app.js
+++ app.js
@@ -1,55 +1,55 @@
-require("dotenv").config();
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+require('dotenv').config()
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
-var certificadosRouter = require("./routes/certificados");
-var paginasRouter = require("./routes/paginas");
-var usersRouter = require("./routes/users");
-var mailRouter = require("./routes/email");
-const cors = require("cors");
+var certificadosRouter = require('./routes/certificados')
+var paginasRouter = require('./routes/paginas')
+var usersRouter = require('./routes/users')
+var mailRouter = require('./routes/email')
+const cors = require('cors')
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://portafolio-lac-one.vercel.app"],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "*",
+    origin: ['http://localhost:5173', 'https://portafolio-lac-one.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: '*',
   })
-);
+)
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use("/usuarios", usersRouter);
-app.use("/certificados", certificadosRouter);
-app.use("/paginas", paginasRouter);
-app.use("/enviarEmail", mailRouter);
+app.use('/usuarios', usersRouter)
+app.use('/certificados', certificadosRouter)
+app.use('/paginas', paginasRouter)
+app.use('/enviarEmail', mailRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+  res.status(err.status || 500)
+  res.render('error', { error: err })
+})
 
-module.exports = app;
+module.exports = app
